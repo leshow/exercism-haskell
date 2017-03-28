1c8b448557f82b57f3cd96e1b763ac4d6df6b869
Evan Cameron <evan@vectorface.com>
2017-03-28 13:33:09 -0400

    ETL

--- etl/src/ETL.hs
+++ etl/src/ETL.hs
@@ -1,6 +1,14 @@
 module ETL (transform) where
 
-import Data.Map (Map)
+import           Data.Char (toLower)
+import           Data.Map  (Map)
+import qualified Data.Map  as M
+
 
 transform :: Map a String -> Map Char a
-transform legacyData = error "You need to implement this function."
+transform = M.fromList . concatMap (\(g, xs) ->
+    let
+        list = zip (fmap toLower xs) (repeat g)
+    in
+        list
+    ) . M.toList
