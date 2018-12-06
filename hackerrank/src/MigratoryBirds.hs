08299f88c0f93fee4887af57efe3f1ae7c1c8873
Evan Cameron <evan@vectorface.com>
2018-12-06 16:09:49 -0500

    countingvalleys

--- hackerrank/src/MigratoryBirds.hs
+++ hackerrank/src/MigratoryBirds.hs
@@ -1,5 +1,7 @@
 module MigratoryBirds where
 
+import           GHC.TypeLits
+
 {-
 You have been asked to help study the population of birds migrating across the continent. Each type of bird you are interested in will be identified by an integer value. Each time a particular kind of bird is spotted, its id number will be added to your array of sightings. You would like to be able to find out which type of bird is most common given a list of sightings. Your task is to print the type number of that bird and if two or more types of birds are equally common, choose the type with the smallest ID number.
 
@@ -31,4 +33,5 @@ migratoryBirds arr =
               (0, 0)
         & fst
     where intmap = foldl' (\acc c -> Map.insertWith @Int (+) c 1 acc) Map.empty
+
 
