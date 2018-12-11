4c5ab5b5db0989e3c98f64f1a05a8120d5345b92
Evan Cameron <evan@vectorface.com>
2018-12-11 13:14:26 -0500

    sparsearrays

--- hackerrank/src/SparseArrays.hs
+++ hackerrank/src/SparseArrays.hs
@@ -8,9 +8,9 @@ import           Data.Maybe                          ( fromJust )
 
 matchingStrings :: [String] -> [String] -> [Int]
 matchingStrings strings queries = fromJust doquery
-  where
-    stringMap :: Map String Int
-    stringMap = foldr (\s acc -> Map.insertWith (+) s 1 acc) Map.empty strings
-    doquery   = forM queries $ \query -> case Map.lookup query stringMap of
-        Just count -> Just count
-        Nothing    -> Just 0
+ where
+  stringMap :: Map String Int
+  stringMap = foldr (\s acc -> Map.insertWith (+) s 1 acc) Map.empty strings
+  doquery   = forM queries $ \query -> case Map.lookup query stringMap of
+    Just count -> Just count
+    Nothing    -> Just 0
