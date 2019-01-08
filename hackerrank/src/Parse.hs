8d475e412f55fda5a33a64549d880ba2e7cd5188
Evan Cameron <evan@vectorface.com>
2019-01-08 15:07:43 -0500

    parsing w/ monadplus

---
+++ hackerrank/src/Parse.hs
@@ -0,0 +1,32 @@
+module Parse where
+
+import           Control.Monad
+import           Data.Char
+import           Control.Applicative
+
+
+data Parsed = Digit Integer | Hex Integer | Word String deriving Show
+
+parseHex :: Parsed -> Char -> [Parsed]
+parseHex (Hex i) c = if isHexDigit c
+    then return (Hex ((i * 16) + toInteger (digitToInt c)))
+    else mzero
+parseHex _ _ = mzero
+
+parseDigit :: Parsed -> Char -> [Parsed]
+parseDigit (Digit i) c = if isDigit c
+    then return (Digit ((i * 10) + toInteger (digitToInt c)))
+    else mzero
+parseDigit _ _ = mzero
+
+parseWord :: Parsed -> Char -> [Parsed]
+parseWord (Word s) c = if isAlpha c then return (Word (s <> [c])) else mzero
+parseWord _        _ = mzero
+
+parse :: Parsed -> Char -> [Parsed]
+parse p c = parseHex p c <|> parseDigit p c <|> parseWord p c
+
+parseArg :: String -> [Parsed]
+parseArg s = do
+    initial <- pure (Hex 0) <|> pure (Digit 0) <|> pure (Word "")
+    foldM parse initial s
