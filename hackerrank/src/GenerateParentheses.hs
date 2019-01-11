af26c23edfb2ece8f288cd1b233dc3afc61477f5
Evan Cameron <evan@vectorface.com>
2019-01-11 14:26:01 -0500

    add overloadedlabels

--- hackerrank/src/GenerateParentheses.hs
+++ hackerrank/src/GenerateParentheses.hs
@@ -17,11 +17,10 @@ For example, given n = 3, a solution set is:
 generateParentheses :: Int -> [String]
 generateParentheses 0 = [""]
 generateParentheses n =
-    [ "(" ++ x ++ ")" ++ y
-    | m <- [0 .. n - 1]
-    , x <- generateParentheses m
-    , y <- generateParentheses (n - 1 - m)
-    ]
+  [ "(" ++ x ++ ")" ++ y
+  | m <- [0 .. n - 1]
+  , x <- generateParentheses m
+  , y <- generateParentheses (n - 1 - m)
+  ]
     -- 0 <= m <= n - 1
     -- 0 <= n - 1 - m
-
