3147dc243404fac12bb8ac0bd644a07ebdaad7df
Evan Cameron <cameron.evan@gmail.com>
2019-01-15 00:07:48 -0500

    license key

---
+++ hackerrank/src/LicenseKeys.hs
@@ -0,0 +1,10 @@
+module LicenseKeys where
+
+import           Data.List                           ( intercalate )
+
+licenseKeys :: Int -> String -> String
+licenseKeys k = intercalate "-" . splitN k . filter (/= '-')
+
+splitN :: Int -> [a] -> [[a]]
+splitN _ [] = []
+splitN n xs = take n xs : splitN n (drop n xs)
