0930f37d1c4d8a181b0fa4cbf4bdcfb451954cf4
Evan Cameron <cameron.evan@gmail.com>
2019-02-05 00:36:09 -0500

    generateHashtag submitted

--- hackerrank/src/HastagGenerator.hs
+++ hackerrank/src/HastagGenerator.hs
@@ -1,13 +1,20 @@
 module HashtagGenerator where
 
 import           Data.Char
+import           Control.Monad
 
 generateHashtag :: String -> Maybe String
 generateHashtag s =
-    let nospace = length $ filter (== ' ') s
-    in  if nospace > 140 || nospace == 0
+    let nospace = length $ filter (/= ' ') s
+        str     = concatMap (\(c : cs) -> toUpper c : cs) $ words s
+    in  if nospace > 140 || nospace == 0 || null str
             then Nothing
-            else
-                Just
-                $  "#"
-                <> (concatMap (\(c : cs) -> toUpper c : cs) $ words s)
+            else Just $ '#' : str
+
+generateHashtag' :: String -> Maybe String
+generateHashtag' s = do
+    let nospace = length $ filter (/= ' ') s
+        str     = concatMap caps $ words s
+    guard (nospace <= 140 || nospace /= 0 || not (null str))
+    pure $ '#' : str
+    where caps (c : cs) = toUpper c : cs
