d66d91871e1c1a3e44b0754a0a959254e39b212e
Evan Cameron <cameron.evan@gmail.com>
2019-02-13 17:25:48 -0500

    designpdfviewer hackerrank

---
+++ hackerrank/src/DesignPDFViewer.hs
@@ -0,0 +1,10 @@
+module PDFViewer where
+
+import qualified Data.Map                           as Map
+
+-- Complete the designerPdfViewer function below.
+designerPdfViewer :: [Int] -> String -> Int
+designerPdfViewer h word = tallest * length word
+  where
+    abcMap = Map.fromList $ zip ['a' .. 'z'] h
+    tallest = foldr (\c m -> max m (Map.findWithDefault 0 c abcMap)) 0 word
