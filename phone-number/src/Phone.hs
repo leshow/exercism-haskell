7c365be946f1bc34b444c5bbe05f27e23ac03543
Evan Cameron <cameron.evan@gmail.com>
2019-09-08 23:44:54 -0400

    Simplified

--- phone-number/src/Phone.hs
+++ phone-number/src/Phone.hs
@@ -7,38 +7,23 @@ import           Data.Char
 import           Control.Monad
 
 number :: String -> Maybe String
-number xs =
-    let
-        nums = filter isNumber xs
-        len  = length nums
-    in
-        do
-            guard (null clean || not anyLetters)
-            guard allowedSymbols
-            guard (len <= 11)
-            guard (len /= 9)
-            join $ pure $ if len == 11
-                then
-                    let (c, a : _, e : _) = getLong nums
-                        cnum              = read c :: Int
-                    in  do
-                            guard (cnum `notElem` [0, 2, 3, 4, 5, 6, 7, 8, 9])
-                            guard (a /= '0' && a /= '1' && e /= '0' && e /= '1')
-                            pure (tail nums)
-                else
-                    let (a : _, e : _) = getShort nums
-                    in  do
-                            guard (a /= '0' && a /= '1' && e /= '0' && e /= '1')
-                            pure nums
+number xs = do
+    let nums = filter isDigit xs
+    let len  = length nums
+    guard (len == 10 || head nums == '1')
+    join $ pure $ if len == 11
+        then do
+            let (c, a : _, e : _) = getLong nums
+            let cnum              = read c :: Int
+            guard (cnum `notElem` [2 .. 9])
+            guard (a `elem` ['2' .. '9'])
+            guard (e `elem` ['2' .. '9'])
+            pure (tail nums)
+        else do
+            let (a : _, e : _) = getShort nums
+            guard (a `elem` ['2' .. '9'])
+            guard (e `elem` ['2' .. '9'])
+            pure nums
   where
-    anyLetters = any (\c -> isLetter c && c /= ' ') clean
-    allowedSymbols =
-        null clean
-            || any
-                   (\c ->
-                       c /= ')' || c /= '(' || c /= '+' || c /= '.' || c /= ' '
-                   )
-                   clean
     getLong s = (take 1 s, take 3 (drop 1 s), take 3 (drop 4 s))
     getShort s = (take 3 s, take 3 (drop 3 s))
-    clean = filter (not . isNumber) xs
