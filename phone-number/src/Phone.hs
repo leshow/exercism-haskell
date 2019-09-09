4f09ad78151bd59f81aab49ae79b4c11147e7c40
Evan Cameron <cameron.evan@gmail.com>
2019-09-08 23:30:51 -0400

    Phone example took forever, Parser soln probably nicer

--- phone-number/src/Phone.hs
+++ phone-number/src/Phone.hs
@@ -1,20 +1,44 @@
-module Phone (number) where
+module Phone
+    ( number
+    )
+where
 
-import           Data.Char (isAlpha, isDigit, isSpace)
+import           Data.Char
+import           Control.Monad
 
 number :: String -> Maybe String
-number xs
-    | fmap (==10) len == Just True = nums
-    | fmap (==11) len == Just True && y == '1' = tail <$> nums
-    | otherwise = Nothing
-    where
-        nums = filter isDigit <$> traverse doChar xs
-        (Just y) = head <$> nums
-        len = length <$> nums
-
-doChar :: Char -> Maybe Char
-doChar c
-    | isDigit c = Just c
-    | isSpace c || c == '.' || c == '(' || c == ')' || c == '-' = Just ' '
-    | isAlpha c = Nothing
-    | otherwise = Nothing
+number xs =
+    let
+        nums = filter isNumber xs
+        len  = length nums
+    in
+        do
+            guard (null clean || not anyLetters)
+            guard allowedSymbols
+            guard (len <= 11)
+            guard (len /= 9)
+            join $ pure $ if len == 11
+                then
+                    let (c, a : _, e : _) = getLong nums
+                        cnum              = read c :: Int
+                    in  do
+                            guard (cnum `notElem` [0, 2, 3, 4, 5, 6, 7, 8, 9])
+                            guard (a /= '0' && a /= '1' && e /= '0' && e /= '1')
+                            pure (tail nums)
+                else
+                    let (a : _, e : _) = getShort nums
+                    in  do
+                            guard (a /= '0' && a /= '1' && e /= '0' && e /= '1')
+                            pure nums
+  where
+    anyLetters = any (\c -> isLetter c && c /= ' ') clean
+    allowedSymbols =
+        null clean
+            || any
+                   (\c ->
+                       c /= ')' || c /= '(' || c /= '+' || c /= '.' || c /= ' '
+                   )
+                   clean
+    getLong s = (take 1 s, take 3 (drop 1 s), take 3 (drop 4 s))
+    getShort s = (take 3 s, take 3 (drop 3 s))
+    clean = filter (not . isNumber) xs
