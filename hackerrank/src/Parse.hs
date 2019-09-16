d11c6ab2d8b9b4d3701c71b81f3d668b15d0a4cf
Evan Cameron <cameron.evan@gmail.com>
2019-09-16 01:34:54 -0400

    two city scheduling

--- hackerrank/src/Parse.hs
+++ hackerrank/src/Parse.hs
@@ -30,3 +30,13 @@ parseArg :: String -> [Parsed]
 parseArg s = do
     initial <- pure (Hex 0) <|> pure (Digit 0) <|> pure (Word "")
     foldM parse initial s
+
+twoCitySched :: [[Int]] -> Int
+twoCitySched costs = go costs n n
+  where
+    go [] _ _ = 0
+    go ((x : y : []) : xs) a b =
+        let a' = if a > 0 then x + go xs (a - 1) b else maxBound
+            b' = if b > 0 then y + go xs a (b - 1) else maxBound
+        in  min a' b'
+    n = length costs `div` 2
