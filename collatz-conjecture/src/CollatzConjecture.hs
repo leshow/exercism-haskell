9d2fedba8409eb2d7852096e1f39d12b5ced4138
Evan Cameron <cameron.evan@gmail.com>
2019-11-19 23:47:12 -0500

    Collatz conjecture

---
+++ collatz-conjecture/src/CollatzConjecture.hs
@@ -0,0 +1,11 @@
+module CollatzConjecture
+    ( collatz
+    )
+where
+
+collatz :: Integer -> Maybe Integer
+collatz n | n <= 0    = Nothing
+          | n == 1    = Just 0
+          | even n    = fmap (+ 1) (collatz $ n `div` 2)
+          | odd n     = fmap (+ 1) (collatz $ 3 * n + 1)
+          | otherwise = Nothing
